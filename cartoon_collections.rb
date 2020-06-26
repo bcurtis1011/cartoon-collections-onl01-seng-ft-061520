49a0c9dfeb8b1799ae2459b1eccc2ba2936cb96c
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-26 17:57:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,8 @@ def long_planeteer_calls(calls_array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
